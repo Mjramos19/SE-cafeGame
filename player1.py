81220495fb498053186c8a0815ce51988ba72f51
Taylor Ulve <ulvet@mail.gvsu.edu>
2026-02-18 19:13:40 -0500

    made player1 file and put player class in it

---
+++ player1.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+
+
+class Player:
+    def __init__(self, width, height):
+        self.x, self.y = 40, 600
+        self.speed = 5
+        self.width = width
+        self.height = height
+
+    def get_rect(self):
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def handle_movement(self, keys, collisions):
+        # Store old position in case we hit something
+        old_x, old_y = self.x, self.y
+
+        # Move
+        if keys[pygame.K_LEFT]:  self.x -= self.speed
+        if keys[pygame.K_RIGHT]: self.x += self.speed
+        if keys[pygame.K_UP]:    self.y -= self.speed
+        if keys[pygame.K_DOWN]:  self.y += self.speed
+
+        # Collision Check: If new position overlaps obstacle, move back
+        for c in collisions:
+            if self.get_rect().colliderect(c):
+                self.x, self.y = old_x, old_y
+
+    def take_order(self):
+        pass
+
+    def attempt_restock(self):
+        pass
+
+    def attempt_brew(self):
+        pass
+
+    def deliver(self):
+        pass
+
+    def render(self):
+        return pygame.draw.rect(screen, COLORS['RED'], player.get_rect())
+
